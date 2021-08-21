7a63c4830ce9e0370cf34ce0a9cf9966436ce487
Alka singh <alkasingh6feb@gmail.com>
2021-08-21 17:29:20 +0530

    commmit with .env deleted
    Signed-off-by: Alka singh <alkasingh6feb@gmail.com>

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,11 +1,11 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => 'e54c2384e4bb4fc227b9995eb36abdbf17a484bf',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -413,12 +413,12 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'e54c2384e4bb4fc227b9995eb36abdbf17a484bf',
             'dev_requirement' => false,
         ),
         'laravel/legacy-factories' => array(

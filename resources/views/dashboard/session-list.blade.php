5133e93c43a9746cd3bd7ef882660896c90dcc07
Anand Shukla <anandkshuklap@gmail.com>
2021-08-23 14:33:43 +0530

    topic error

--- resources/views/dashboard/session-list.blade.php
+++ resources/views/dashboard/session-list.blade.php
@@ -294,42 +294,29 @@
                             <div id="accordion" >
                                 <div class="card mb-3">
                                     <div class="card-header" id="headingThree">
-                                        @foreach($batches as $batch)
-                                        @foreach($batch->batchSession as $session)
+                                        @foreach($allsessions as $session)
+                                      
                                         <div class="card-header" id="headingOne">
                                             <h5 class="mb-0">
                                                 <div class="d-flex justify-content-around">
                                                     <div class="card_img" style="width:38%">
-                                                                             @if($batch->subject->name == 'English')
-                                
-                                  <img style="width:100%;" src="{{ asset('frontend/assets/English/English.jpg') }}" alt="">
-                                
-                                @elseif($batch->subject->name == 'Maths')
-                                
-                                    <img  style="width:100%;" src="{{ asset('frontend/assets/Maths/Math.jpg') }}" alt="">
-                                
-                                @elseif($batch->subject->name == 'Physics')
-                                
-                                    <img  style="width:100%;" src="{{ asset('frontend/assets/Physics/Physics.jpg') }}" alt="">
-                                
-                                @elseif($batch->subject->name == 'Chemistry')
-                                
-                                    <img style="width:100%" src="{{ asset('frontend/assets/card-cover.png') }}" alt="">
-                                
-                                @elseif($batch->subject->name == 'Essay')
-                                
-                                    <img  style="width:100%" src="{{ asset('frontend/assets/Essay/Essay.jpg') }}" alt="">
-                                
-                                @else
-                                
-                                <img style="width:100%" src="{{ asset('frontend/assets/English/English.jpg')}}" alt="">
-                                
-                                @endif
-
+                                                        @if($session->batch->subject->name == 'English')
+                                                            <img style="width:100%;" src="{{ asset('frontend/assets/English/English.jpg') }}" alt="">
+                                                            @elseif($session->batch->subject->name == 'Maths')  
+                                                                <img  style="width:100%;" src="{{ asset('frontend/assets/Maths/Math.jpg') }}" alt="">
+                                                            @elseif($session->batch->subject->name == 'Physics')
+                                                                <img  style="width:100%;" src="{{ asset('frontend/assets/Physics/Physics.jpg') }}" alt="">
+                                                            @elseif($session->batch->subject->name == 'Chemistry')
+                                                                <img style="width:100%" src="{{ asset('frontend/assets/card-cover.png') }}" alt="">
+                                                            @elseif($session->batch->subject->name == 'Essay')
+                                                                <img  style="width:100%" src="{{ asset('frontend/assets/Essay/Essay.jpg') }}" alt="">
+                                                            @else
+                                                                <img style="width:100%" src="{{ asset('frontend/assets/English/English.jpg')}}" alt="">
+                                                            @endif
                                                     </div>
                                                     <div>
                                                         <p class="subject_card">Subject</p>
-                                                        <p style="font-size:15px;" class="card_subject_title1">{{$batch->subject->name}}</p>
+                                                        <p style="font-size:15px;" class="card_subject_title1">{{$session->batch->subject->name}}</p>
                                                     </div>
                                                     <div class="border-left first_left_border"></div>
                                                     <div>
@@ -350,7 +337,7 @@
                                             </h5>
                                         </div>
 
-                                        <div id="collapse{{$session->id}}" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                                        <div id="collapse{{$session->id}}" class="collapse" aria-labelledby="headingOne" data-parent="#headingOne">
                                             <div class="card-body">
 
                                                 <table class="table">
@@ -365,10 +352,6 @@
 
                                                     <tr>
                                                         <td>Topics</td>
-                                                        {{-- <td> @foreach($session->topics as $t)
-                                                            {{ $session->name }} - {{ $t->topic->name }}
-                                                        </td>
-                                                        @endforeach --}}
                                                         <td>{{$session->name}}-
                                                         @if(isset($session->topics_name->topic->name))
                                                         {{$session->topics_name->topic->name}}
@@ -381,20 +364,9 @@
                                                     <tr>
                                                         <td colspan="2"><a href="{{route('home-work', ['id'=>$session->id])}}">View Homework</a></td>
                                                     </tr>
-                                                   {{-- <tr>
-                                                    <td>Due Amount</td>
-                                                    <td>
-                                                        @php
-                                                        $items = $batch->items->pluck('order_payment_id');
-                                                        $payments = App\Models\OrderPayment::whereIn('id', $items)->where('student_id', auth()->id())->first();
-                                                        @endphp
-                                                        &euro; {{ $payments->order_amount - $payments->paid_amount }}
-                                                    </td>
-                                                </tr>--}}
                                                 </table>
                                             </div>
                                         </div>
-                                        @endforeach
                                         @endforeach
                                     </div>
                                 </div>
@@ -618,7 +590,12 @@
                                         {{ $session->name }} - {{ $t->topic->name }}
                                     </td>
                                  @endforeach --}}
-                                 <td>{{$session->topics_name->topic->name}}</td>
+                                 <td>@if(isset($session->topics_name->topic->name))
+                                    {{$session->topics_name->topic->name}}
+                                    
+                                    @else
+                                        Error in Topic
+                                    @endif</td>
 
                             </tr>
                                 <!--  <td>-->

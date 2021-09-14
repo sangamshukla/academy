526810debcafbef18a9bdcc1e410168899c346e6
Anand Shukla <anandkshuklap@gmail.com>
2021-09-14 11:18:44 +0530

    bug fixes

--- app/Http/Controllers/HomeWorkController.php
+++ app/Http/Controllers/HomeWorkController.php
@@ -470,7 +470,7 @@ class HomeWorkController extends Controller
         } else {
             return back()->with('msg', 'There was some error');
         }
-            ]);
+            
             if($save_checked_homework)
             {
                 return back()->with('msg', 'Your checked home has been submitted');

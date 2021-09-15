be3243f2f26062f8d146afdfb5cf29676ad15f45
Anand Shukla <anandkshuklap@gmail.com>
2021-09-15 10:37:06 +0530

    scoresheet error

--- resources/views/offlinescoresheet/newscoresheetpdf.blade.php
+++ resources/views/offlinescoresheet/newscoresheetpdf.blade.php
@@ -131,7 +131,7 @@
                   @endphp
                    <tr class="table-td">
                     <th scope="row" class="header-para" class="header-para">1</th>
-                    <td class="header-para">{{ $class_average_marks }}</td>
+                    <td class="header-para">{{ number_format($class_average_marks) }}</td>
                     <td class="header-para">
                       @php
                           $maximum=max($class_highest_marks);
@@ -140,13 +140,13 @@
                     </td>
                     <td class="header-para">
                       @php
-                          $maximum=min($class_highest_marks);
-                          echo $maximum;
+                          $minimum=min($class_highest_marks);
+                          echo number_format($minimum);
                       @endphp
                     </td>
                     {{-- <td class="header-para">12</td> --}}
                     <td class="header-para">77% </td>
-                    <td class="header-para">{{$student_marks}}</td>
+                    <td class="header-para">{{number_format($student_marks)}}</td>
                   </tr>
                 </tbody>
               </table>
@@ -186,12 +186,12 @@
                       @endphp
                   <tr>
                     <th scope="row" class="header-para">1</th>
-                    <td class="header-para">{{$class_average_marks}}</td>
+                    <td class="header-para">{{number_format($class_average_marks)}}</td>
                     <td class="header-para">
-                          {{$class_highest_marks}}
+                          {{number_format($class_highest_marks)}}
                     </td>
-                    <td class="header-para"> {{$class_lowest_marks}}</td>
-                    <td class="header-para">{{$student_marks->obtained_marks}}/{{$student_marks->full_marks}}</td>
+                    <td class="header-para"> {{number_format($class_lowest_marks)}}</td>
+                    <td class="header-para">{{number_format($student_marks->obtained_marks)}}/{{number_format($student_marks->full_marks)}}</td>
                     {{-- <td class="header-para">2/10</td> --}}
                     <td class="header-para">{{$student_percentage}}%</td>
                   </tr>

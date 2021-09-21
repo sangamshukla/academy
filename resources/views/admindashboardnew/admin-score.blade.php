6986fdaae752f5c910d1db300208f1789c9c0d60
Anand Shukla <anandkshuklap@gmail.com>
2021-09-22 02:12:21 +0530

    scores minor fixes

--- resources/views/admindashboardnew/admin-score.blade.php
+++ resources/views/admindashboardnew/admin-score.blade.php
@@ -2,38 +2,36 @@
 
 @section('content')
     <div>
-          @if (count($weeks)>1)
-        <select name="select_week" id="week_id_name">
-            @foreach ($weeks as $week)
-                <option value="{{ $week->id }}">{{ $week->week_name }}</option>
-            @endforeach
-        </select>
-        <input type="button" value="Get Scoresheet" id="get-admin-score" onclick="get_score();">
+        @if (count($weeks) > 1)
+            <select name="select_week" id="week_id_name">
+                @foreach ($weeks as $week)
+                    <option value="{{ $week->id }}">{{ $week->week_name }}</option>
+                @endforeach
+            </select>
+            <input type="button" value="Get Scoresheet" id="get-admin-score" onclick="get_score();">
     </div>
     <br>
-     @else
-        <p>No exam has been held so far</p>
     @endif
-    @if (count($weeks)>1)
-        
-    
-    <div id="admin-score-table">
-        {{-- @include('admindashboardnew.admin-score-table') --}}
-        <table class="table table-bordered" id="users-table">
-            <thead>
-                <tr>
-                    <th>Student</th>
-                    @foreach ($subject_full_marks as $subject_full_mark)
-                        <th scope="col">
-                            {{ $subject_full_mark->subject->name . '(' . round($subject_full_mark->full_marks) . ')' }}
-                        </th>
-                        <th>Rank</th>
-                    @endforeach
-                </tr>
-            </thead>
-        </table>
+    @if (count($weeks) > 1)
 
-    </div>
+
+        <div id="admin-score-table">
+            {{-- @include('admindashboardnew.admin-score-table') --}}
+            <table class="table table-bordered" id="users-table">
+                <thead>
+                    <tr>
+                        <th>Student</th>
+                        @foreach ($subject_full_marks as $subject_full_mark)
+                            <th scope="col">
+                                {{ $subject_full_mark->subject->name . '(' . round($subject_full_mark->full_marks) . ')' }}
+                            </th>
+                            <th>Rank</th>
+                        @endforeach
+                    </tr>
+                </thead>
+            </table>
+
+        </div>
     @else
         <p>No exam has been held so far</p>
     @endif

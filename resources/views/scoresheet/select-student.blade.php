018c6240638e99dba3b6dc8366a26618aad13ce8
Anand Shukla <anandkshuklap@gmail.com>
2021-09-27 19:00:16 +0530

    made databtable

--- resources/views/scoresheet/select-student.blade.php
+++ resources/views/scoresheet/select-student.blade.php
@@ -1,29 +1,39 @@
 @extends('layouts.admin_dashboard')
 @section('content')
+
     Select Student for Scoresheet {{ $id }}
-    <table class="table table-hover table-inverse table-responsive">
-        <thead class="thead-inverse">
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Select</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($students as $student)
+    <form action="{{ route('select-student') }}" method="post">
+        @csrf
+        <table class="table table-hover table-inverse table-responsive" id="select-student">
+            <thead class="thead-inverse">
                 <tr>
-                    <td scope="row">{{ $student->name }}</td>
-                    <td>{{ $student->email }}</td>
-                    <form action="{{ route('select-student') }}" method="post">
-                        @csrf
-                        <td><input type="checkbox" name="selected_students[]" value="{{ $student->id }}"></td>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Select</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <input type="hidden" name="scoresheet_id" value="{{ $id }}">
-    <div class="text-center">
-        <input class="btn btn-primary center" type="submit" value="Submit">
-    </div>
+            </thead>
+            <tbody>
+                @foreach ($students as $student)
+                    <tr>
+                        <td scope="row">{{ $student->name }}</td>
+                        <td>{{ $student->email }}</td>
+
+                        <td><input type="checkbox" name="selected_students[]" value="{{ $student->id }}"></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <input type="hidden" name="scoresheet_id" value="{{ $id }}">
+        <div class="">
+            <input class=" btn btn-primary center" type="submit" value="Submit">
+        </div>
     </form>
+    @push('scripts')
+        <script>
+            $(document).ready(function() {
+                $('#select-student').DataTable();
+            });
+        </script>
+
+    @endpush
 @endsection
